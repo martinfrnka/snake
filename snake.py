a870c40cea860f5e82082951633aefece1fd2b64
martinfrnka <martin.frnka@gmail.com>
2022-07-25 13:48:38 +0200

    added object player updated code to use object

--- snake.py
+++ snake.py
@@ -1,5 +1,19 @@
 import time
 import pygame
+
+class Player():
+    def __init__(self,x,y):
+        self.posx = x
+        self.posy = y
+        self.dx = 0
+        self.dy = 0
+        self.size = 10
+        self.speed = 30
+
+    def getObject(self):
+        return [self.posx, self.posy, self.size, self.size]
+
+    
 pygame.init()
 
 white=(255,255,255)
@@ -13,16 +27,17 @@ pygame.display.set_caption('Snake game by Martin')
 
 game_over=False
 
-x1 = dis_w/2
-y1 = dis_h/2
+xx1 = dis_w/2
+xy1 = dis_h/2
+snake = Player(100,100)
 
-snake_block = 10
+xsnake_block = 10
 
-x1_change = 0
-y1_change = 0
+xx1_change = 0
+xy1_change = 0
 
 clock = pygame.time.Clock()
-snake_speed = 30
+xsnake_speed = 30
 
 font_style = pygame.font.SysFont(None, 50)
 
@@ -30,35 +45,39 @@ def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg,[dis_w/2, dis_h/2])
 
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                x1_change = -10
-                y1_change = 0
+                snake.dx = -10
+                snake.dy = 0
             elif event.key == pygame.K_RIGHT:
-                x1_change = 10
-                y1_change = 0
+                snake.dx = 10
+                snake.dy = 0
             elif event.key == pygame.K_UP:
-                x1_change = 0
-                y1_change = -10
+                snake.dx = 0
+                snake.dy = -10
             elif event.key == pygame.K_DOWN:
-                x1_change = 0
-                y1_change = 10
+                snake.dx = 0
+                snake.dy = 10
             elif event.key == pygame.K_ESCAPE:
                 game_over = True
 
-    x1 += x1_change
-    y1 += y1_change
-    if (x1>=dis_w-snake_block or x1<=0 or y1>=dis_h-snake_block or y1 <= 0):
+    snake.posx += snake.dx
+    snake.posy += snake.dy
+    if (snake.posx>=dis_w-snake.size
+        or snake.posx<=0
+        or snake.posy>=dis_h-snake.size
+        or snake.posy <= 0):
         game_over = True
     dis.fill(white)
-    pygame.draw.rect(dis,black,[x1,y1,snake_block,snake_block])
+    pygame.draw.rect(dis,black,snake.getObject())
     pygame.display.update()
 
-    clock.tick(snake_speed)
+    clock.tick(snake.speed)
 
 message("You lost!", red)
 pygame.display.update()
